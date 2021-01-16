d36f1fafe43acda0a3edd4389e90d70983766ca6
teachwiki <35622257+teachwiki@users.noreply.github.com>
2021-01-16 04:04:03 -0800

    Add files via upload

---
+++ core/eventListener.js
@@ -0,0 +1,78 @@
+export const addEventListener = (event, eventListener) => {
+  const eventToObjectMap = getEventToObjectMap();
+  const object = eventToObjectMap[event];
+
+  object.on(event, eventListener);
+};
+
+export const removeEventListener = (event, eventListener) => {
+  const eventToObjectMap = getEventToObjectMap();
+  const object = eventToObjectMap[event];
+
+  object.off(event, eventListener);
+};
+
+const getEventToObjectMap = () => {
+  const annotManager = window.docViewer.getAnnotationManager();
+  const historyManager = window.docViewer.getAnnotationHistoryManager();
+  const editBoxManager = annotManager.getEditBoxManager();
+  const widgetEditingManager = annotManager.getWidgetEditingManager();
+
+  return {
+    signatureSaved: window.docViewer,
+    signatureDeleted: window.docViewer,
+    annotationsLoaded: window.docViewer,
+    changePage: window.docViewer,
+    click: window.docViewer,
+    dblClick: window.docViewer,
+    displayPageLocation: window.docViewer,
+    keyDown: window.docViewer,
+    keyUp: window.docViewer,
+    mouseEnter: window.docViewer,
+    mouseLeave: window.docViewer,
+    mouseLeftDown: window.docViewer,
+    mouseLeftUp: window.docViewer,
+    mouseMove: window.docViewer,
+    mouseRightDown: window.docViewer,
+    mouseRightUp: window.docViewer,
+    pageComplete: window.docViewer,
+    searchInProgress: window.docViewer,
+    activeSearchResultChanged: window.docViewer,
+    searchResultsChanged: window.docViewer,
+    textSelected: window.docViewer,
+    beginRendering: window.docViewer,
+    finishedRendering: window.docViewer,
+    beforeDocumentLoaded: window.docViewer,
+    displayModeUpdated: window.docViewer,
+    documentLoaded: window.docViewer,
+    documentUnloaded: window.docViewer,
+    fitModeUpdated: window.docViewer,
+    rotationUpdated: window.docViewer,
+    toolUpdated: window.docViewer,
+    toolModeUpdated: window.docViewer,
+    zoomUpdated: window.docViewer,
+    pageNumberUpdated: window.docViewer,
+    layoutChanged: window.docViewer,
+    'fitModeUpdated.fitbutton': window.docViewer,
+    historyChanged: historyManager,
+    annotationSelected: annotManager,
+    annotationChanged: annotManager,
+    updateAnnotationPermission: annotManager,
+    addReply: annotManager,
+    deleteReply: annotManager,
+    annotationHidden: annotManager,
+    annotationDoubleClicked: annotManager,
+    annotationFiltered: annotManager,
+    annotationToggled: annotManager,
+    fieldChanged: annotManager,
+    notify: annotManager,
+    setNoteText: annotManager,
+    fileAttachmentDataAvailable: annotManager,
+    editorFocus: editBoxManager,
+    editorBlur: editBoxManager,
+    editorTextChanged: editBoxManager,
+    editorSelectionChanged: editBoxManager,
+    editingStarted: widgetEditingManager,
+    editingEnded: widgetEditingManager,
+  };
+};
---
+++ core/getCurrentPage.js
@@ -0,0 +1,4 @@
+/**
+ * https://www.pdftron.com/api/web/CoreControls.DocumentViewer.html#getCurrentPage__anchor
+ */
+export default () => window.docViewer.getCurrentPage();
