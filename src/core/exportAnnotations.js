bfae78147372cb022ac2e4d021d922a5f65f62e0
teachwiki <35622257+teachwiki@users.noreply.github.com>
2021-01-16 03:59:45 -0800

    Add files via upload

---
+++ src/core/exportAnnotations.js
@@ -0,0 +1,4 @@
+/**
+ * https://www.pdftron.com/api/web/CoreControls.AnnotationManager.html#exportAnnotations__anchor
+ */
+export default options => window.docViewer.getAnnotationManager().exportAnnotations(options);
