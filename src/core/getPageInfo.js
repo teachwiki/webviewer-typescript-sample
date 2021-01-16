6bb7aa49790a58b0c64e556c5f028c049ed03c00
teachwiki <35622257+teachwiki@users.noreply.github.com>
2021-01-16 02:38:44 -0800

    Add files via upload

---
+++ src/core/getPageInfo.js
@@ -0,0 +1,4 @@
+/**
+ * https://www.pdftron.com/api/web/CoreControls.Document.html#getPageInfo__anchor
+ */
+export default pageNumber => window.docViewer.getDocument().getPageInfo(pageNumber);
---
+++ src/core/getSearchMode.js
@@ -0,0 +1,1 @@
+export default () => window.CoreControls.Search.Mode;
